dcaf3b1e1d97629244d2be7ae47ce89d006085d1
umeshredd <47346567+umeshredd@users.noreply.github.com>
2019-05-28 11:47:54 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
-export const ENDPOINT = "https://cloud-back-rdg7uidwzq-uc.a.run.app";
-export const LIKEEND = "https://cloud-likeend-rdg7uidwzq-uc.a.run.app";
-export const COMMENTEND = "https://cloud-commentend-rdg7uidwzq-uc.a.run.app";
+export const ENDPOINT = "https://cloud-back-rdg7uidwzq-uc.a.run.app/";
+export const LIKEEND = "https://cloud-likeend-rdg7uidwzq-uc.a.run.app/";
+export const COMMENTEND = "https://cloud-commentend-rdg7uidwzq-uc.a.run.app/";
 
 // export default {
 //     ENDPOINT,
