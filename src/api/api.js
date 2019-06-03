fb126afffb4ac689f5ab3bdf3286ad0a5e9d9432
umeshredd <47346567+umeshredd@users.noreply.github.com>
2019-06-03 18:28:11 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
-export const ENDPOINT = "https://cloud-back-rdg7uidwzq-uc.a.run.app/";
-export const LIKEEND = "https://cloud-likeend-rdg7uidwzq-uc.a.run.app/";
-export const COMMENTEND = "https://cloud-commentend-rdg7uidwzq-uc.a.run.app/";
+// export const ENDPOINT = "https://cloud-back-rdg7uidwzq-uc.a.run.app/";
+// export const LIKEEND = "https://cloud-likeend-rdg7uidwzq-uc.a.run.app/";
+// export const COMMENTEND = "https://cloud-commentend-rdg7uidwzq-uc.a.run.app/";
 
 // export default {
 //     ENDPOINT,
@@ -10,3 +10,11 @@ export const COMMENTEND = "https://cloud-commentend-rdg7uidwzq-uc.a.run.app/";
 // export const ENDPOINT = "http://34.80.174.41/";
 // export const LIKEEND = "http://34.80.174.41/backend-like";
 // export const COMMENTEND = "http://34.80.174.41/backend-comment";
+
+
+export const ENDPOINT = "https://cloud-back-rdg7uidwzq-uc.a.run.app/";
+export const LIKEEND = "http://35.192.232.226/backend-like/";
+export const COMMENTEND = "http://35.192.232.226/backend-comment/"
+
+
+// http://35.192.232.226/backend-comment
